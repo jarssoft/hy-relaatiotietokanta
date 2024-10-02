a7b353cf5ea4439c3ea9f84c662e5bd92a44a247
Jari Saari <jari.m.saari@gmail.com>
2024-10-02 23:06:27 +0300

    use model

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 
 const express = require('express')
 const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
+class Blog extends Model {}
+
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+
 app.get('/api/blogs', async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+  //const notes = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+  const notes = await Blog.findAll()
   res.json(notes)
 })
 
