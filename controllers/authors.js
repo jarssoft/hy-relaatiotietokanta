ec1f6f47efd59dfae4dc2afece50af7b8567979c
Jari Saari <jari.m.saari@gmail.com>
2024-10-08 13:58:30 +0300

    bonus: järjestä tykkäysten perusteella

--- controllers/authors.js
+++ controllers/authors.js
@@ -3,8 +3,12 @@ const router = express.Router()
 const {User, Blog} = require('../models')
 const sequelize = require("sequelize");
 
-router.get('/', async (req, res, next) => {  
-  const users = await Blog.findAll({attributes: ['author', [sequelize.fn('COUNT', sequelize.col('title')), 'n_title'], [sequelize.fn('SUM', sequelize.col('likes')), 'sum_likes']], group:'author'})
+router.get('/', async (req, res, next) => {
+  let order =  [['sum_likes', 'DESC']]
+  const users = await Blog.findAll(
+      {attributes: ['author', [sequelize.fn('COUNT', sequelize.col('title')), 'n_title'], [sequelize.fn('SUM', sequelize.col('likes')), 'sum_likes']], 
+        group:'author',
+        order})
   res.json(users)
 })
 
